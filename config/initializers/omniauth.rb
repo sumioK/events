c11242c273b11d81fd3f3ae0d056966ee8a8a0f6
Sumio K <tap.miwo@gmail.com>
2022-07-24 20:58:39 +0900

    add database

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
-Rails.application.config.middleware.use OmniAuth::Bundler do
+Rails.application.config.middleware.use OmniAuth::Builder do
     if Rails.env.development? || Rails.env.test?
-        provider :github,"YOUR_CLIENT_ID","YOUR_CLIENT_SECRET" 668e071b47891b091e42    
-   else
+        provider :github,"668e071b47891b091e42","fd91be64d82125266bae9925987fca3c5f9aa41d"     
+    else
         provider :github, 
             Rails.application.credentials.github[:client_id],
             Rails.applicationl.credentials.github[:client_secret]
