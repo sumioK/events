8decd9e4d3792b326b7096d77467417f1f36cb8d
Sumio K <tap.miwo@gmail.com>
2022-07-25 06:24:51 +0900

    create form page

--- config/application.rb
+++ config/application.rb
@@ -25,12 +25,5 @@ module Events
     config.load_defaults 6.1
     config.time_zone = "Tokyo"
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
