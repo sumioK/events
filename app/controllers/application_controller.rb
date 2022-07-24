abc8d252d15440c0941a6b3d704c1e6feab4cba0
Sumio K <tap.miwo@gmail.com>
2022-07-24 22:02:23 +0900

    before create form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate
     helper_method :logged_in?
 
 
@@ -7,6 +8,16 @@ class ApplicationController < ActionController::Base
 
 
     def logged_in?
-        !!session[:user_id]
+        !!current_user
+    end
+
+    def current_user
+        return unless session[:user_id]
+        @current_user ||= User.find(session[:user_id])
+    end
+
+    def authenticate
+        return if logged_in?
+        redirect_to root_path, alert:"ログインしてください"
     end
 end
